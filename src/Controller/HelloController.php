e05136ed8e1269bf11f2c6def9bd19944800ecb6
zrayev <dmitryzrayev@gmail.com>
2018-11-18 01:23:59 +0200

    transfer /hello to routes.yaml

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,12 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class HelloController extends AbstractController
 {
     /**
-     * @Route("/hello", name="hello")
+     * @return Response
      */
     public function index()
     {
