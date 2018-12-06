7a6816d9c2c6b93d21fb1de0c796caffa55bc927
Dmitry Zraiev <dmitryzrayev@gmail.com>
2018-12-06 09:56:10 +0200

    add Comment entity

---
+++ src/Entity/Comment.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
+ */
+class Comment
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author_email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $post_id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_date;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $approved_comment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getAuthorName(): ?string
+    {
+        return $this->author_name;
+    }
+
+    public function setAuthorName(string $author_name): self
+    {
+        $this->author_name = $author_name;
+
+        return $this;
+    }
+
+    public function getAuthorEmail(): ?string
+    {
+        return $this->author_email;
+    }
+
+    public function setAuthorEmail(string $author_email): self
+    {
+        $this->author_email = $author_email;
+
+        return $this;
+    }
+
+    public function getPostId(): ?int
+    {
+        return $this->post_id;
+    }
+
+    public function setPostId(int $post_id): self
+    {
+        $this->post_id = $post_id;
+
+        return $this;
+    }
+
+    public function getCreatedDate()
+    {
+        return $this->created_date;
+    }
+
+    public function setCreatedDate($created_date)
+    {
+        $this->created_date = $created_date;
+
+        return $this;
+    }
+
+    public function getApprovedComment(): ?bool
+    {
+        return $this->approved_comment;
+    }
+
+    public function setApprovedComment(bool $approved_comment): self
+    {
+        $this->approved_comment = $approved_comment;
+
+        return $this;
+    }
+}
